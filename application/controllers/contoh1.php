7b5c1745714d5d7805be66b4fe7b67965318a701
sitirahmaalfdlh <sitirahmaalfadillah2@gmail.com>
2021-10-26 17:47:05 +0700

    menambahkan file contoh1.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Siti Rahma Alfadillah <br>
+              saya tinggal di daerah Cikampek";
+    }
+}
